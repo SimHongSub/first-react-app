b23b413f62f811e6ffc798fb1b2cbc764eb25e6e
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 14:59:25 +0900

    Fragement 태그, 자바스크립트 표현식 사용법

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+        <h1>{name} 안녕!</h1>
+        <h2>잘 작동하니?</h2>
+      </Fragment>
   );
 }
 

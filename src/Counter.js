79b320ffd68ffe19768d39979d0fc86933a2a51c
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 16:39:12 +0900

    setState함수 동기식으로 동작하도록 하기 위해 함수를 인자로 넣어줌

--- src/Counter.js
+++ src/Counter.js
@@ -15,7 +15,15 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1

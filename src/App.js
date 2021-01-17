c4aa9ce162868136767d128d285bf92799d28e6a
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 15:08:27 +0900

    undefined 렌더링하지 않기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
 function App() {
-  const name = '리트';
-  return (
-      <div>
-        {name === '리액트' ? (
-            <h1>리액트입니다.</h1>
-        ) : (
-            <h2>리액트가 아닙니다.</h2>
-        )}
-      </div>
-  );
+  const name = undefined;
+  return name || '값이 undefined입니다.';
 }
 
 export default App;

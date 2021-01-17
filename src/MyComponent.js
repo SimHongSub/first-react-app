19a5c70d58626a0c59601fbcbf2680d6b522ba0b
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 15:58:41 +0900

    props children 사용법

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const MyComponent = (props) => {
-  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+  return (
+    <div>
+      안녕하세요, 제 이름은 {props.name}입니다. <br />
+      children 값은 {props.children}입니다.
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {

b5ccd00f9b9c7846792331f734b212e35cacbb31
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 16:00:47 +0900

    비구조화 할당을 이용한 props 전달

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const MyComponent = (props) => {
+const MyComponent = ({ name, children }) => {
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}입니다.
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}입니다.
     </div>
   );
 };

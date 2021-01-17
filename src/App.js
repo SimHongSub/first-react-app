574edb8ac1c78b19eb2d027e954c85fe351fbd03
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 15:46:06 +0900

    class형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  const name = '리액트';
-  return <div className="react">{name}</div>;
+class App extends Component {
+  render() {
+    const name = "리액트";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;

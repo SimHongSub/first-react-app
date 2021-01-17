938ed6e8b32dc0c7a18a8f17465491b5a892a496
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 15:14:32 +0900

    className 사용법

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import './App.css'
 
 function App() {
   const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
-  };
-  return <div style={style}>{name}</div>;
+  return <div className="react">{name}</div>;
 }
 
 export default App;

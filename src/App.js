38ef171d5ee2bd20d87e34e3fd99316964eeb29a
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 16:12:33 +0900

    propTypes를 통한 props 검증

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoriteNumber={10}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
-      children 값은 {children}입니다.
+      children 값은 {children}입니다. <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
 
 MyComponent.defaultProps = {
   name: "기본 이름",
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;

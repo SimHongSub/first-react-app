b0a1cbee596218441319f242c57af547961cf2d4
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 16:34:27 +0900

    생성자 사용하지 않고 state 초기값 설정 방법

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
 
   render() {
     const { number, fixedNumber } = this.state;

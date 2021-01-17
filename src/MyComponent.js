c5734dff8c24c9dcdfec008a94984dc2b7b2e4a8
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 16:17:44 +0900

    class 내부에 defaultProps, propTypes 설정 방법

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: "기본 이름",
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+
   render() {
     const { name, favoriteNumber, children } = this.props;
     return (
@@ -13,14 +22,5 @@ class MyComponent extends Component {
     );
   }
 }
-
-MyComponent.defaultProps = {
-  name: "기본 이름",
-};
-
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired,
-};
 
 export default MyComponent;

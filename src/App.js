9a847a4cef53e631685061f6e93f7ea253330459
SimHongSub <ghdtjq2038@gmail.com>
2021-01-17 15:11:07 +0900

    인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function App() {
-  const name = undefined;
-  return name || '값이 undefined입니다.';
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  };
+  return <div style={style}>{name}</div>;
 }
 
 export default App;

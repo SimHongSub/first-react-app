d9c85de4e87a1cc6204d539a6daaa02635218f36
SimHongSub <ghdtjq2038@gmail.com>
2021-01-18 17:18:26 +0900

    onkeypress 사용법

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -20,6 +20,12 @@ class EventPractice extends Component {
     });
   };
 
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
+
   render() {
     return (
       <div>
@@ -37,6 +43,7 @@ class EventPractice extends Component {
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>

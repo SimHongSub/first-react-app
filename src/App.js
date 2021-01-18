807970a407c2152fd8da9f72c4f7fa0234e8148a
SimHongSub <ghdtjq2038@gmail.com>
2021-01-18 19:04:08 +0900

    DOM id값 사용하지 않고 특정 Input 값 검증하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-  return <EventPractice />;
+  return <ValidationSample />;
 };
 
 export default App;

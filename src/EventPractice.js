e18db459227c25ee966bda7a1c8a4de904cab16d
SimHongSub <ghdtjq2038@gmail.com>
2021-01-18 16:59:32 +0900

    onchange 이벤트 사용법

---
+++ src/EventPractice.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default EventPractice;

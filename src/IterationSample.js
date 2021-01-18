a28399b75ac20400a99ff1c6b6fe9e6e820b0391
SimHongSub <ghdtjq2038@gmail.com>
2021-01-19 03:01:10 +0900

    데이터 제거 기능

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -20,8 +20,16 @@ const IterationSample = () => {
     setNames(nextNames);
     setInputText("");
   };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
 
-  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
 
   return (
     <>

633fe9ec8eefdb4ff5ed011a07f83fbe71010343
SimHongSub <ghdtjq2038@gmail.com>
2021-01-20 22:05:25 +0900

    Hooks - useState 사용법

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React, { Component } from "react";
-import LifeCycleSample from "./chapter7/LifeCycleSample";
-import ErrorBoundary from "./chapter7/ErrorBoundary";
-
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+import React from "react";
+import Counter from "./chapter8/Counter";
+const App = () => {
+  return <Counter />;
+};
 
 export default App;

b6a4110ea1f3ef98e4db0046025383491e40a486
SimHongSub <ghdtjq2038@gmail.com>
2021-01-22 23:47:39 +0900

    sass 사용하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Info from "./chapter8/Info";
+import SassComponent from "./chapter9/SassComponent";
 
 const App = () => {
-  return <Info />;
+  return <SassComponent />;
 };
 
 export default App;

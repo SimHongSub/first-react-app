c6a6fb6185b4f119a2a50f4dd1b7d251d380581c
SimHongSub <ghdtjq2038@gmail.com>
2021-01-26 00:16:17 +0900

    일정 관리 웹 앱 - 항목 삭제 기능 구현

--- src/chapter10/TodoList.js
+++ src/chapter10/TodoList.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./TodoList.scss";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
       ))}
     </div>
   );
--- src/chapter10/TodoListItem.js
+++ src/chapter10/TodoListItem.js
@@ -7,8 +7,8 @@ import {
 import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo }) => {
-  const { text, checked } = todo;
+const TodoListItem = ({ todo, onRemove }) => {
+  const { id, text, checked } = todo;
 
   return (
     <div className="TodoListItem">
@@ -16,7 +16,7 @@ const TodoListItem = ({ todo }) => {
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
-      <div className="remove">
+      <div className="remove" onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>

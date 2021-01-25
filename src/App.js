263428abbaf16dfd4e35bea7bb3c4bc2696c7289
SimHongSub <ghdtjq2038@gmail.com>
2021-01-25 23:57:55 +0900

    todos state 사용한 리스트 뿌리기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./chapter10/TodoTemplate";
 import TodoInsert from "./chapter10/TodoInsert";
 import TodoList from "./chapter10/TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "리액트 기초 알아보기", checked: true },
+    { id: 2, text: "컴포넌트 스타일링해 보기", checked: true },
+    { id: 3, text: "일정 관리 앱 만들어 보기", checked: false },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
--- src/chapter10/TodoList.js
+++ src/chapter10/TodoList.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./TodoList.scss";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };

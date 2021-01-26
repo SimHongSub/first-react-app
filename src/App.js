5c93d558c5bc744071556078cc8a335c4a541cb9
SimHongSub <ghdtjq2038@gmail.com>
2021-01-26 21:19:50 +0900

    immer 적용하기

--- src/App.js
+++ src/App.js
@@ -1,80 +1,8 @@
-import React, { useReducer, useState, useRef, useCallback } from "react";
-import TodoTemplate from "./chapter10/TodoTemplate";
-import TodoInsert from "./chapter10/TodoInsert";
-import TodoList from "./chapter10/TodoList";
-
-function createBulkTodos() {
-  const array = [];
-
-  for (let i = 1; i <= 2500; i++) {
-    array.push({
-      id: i,
-      text: `할 일 ${i}`,
-      checked: false,
-    });
-  }
-
-  return array;
-}
-
-function todoReducer(todos, action) {
-  switch (action.type) {
-    case "INSERT":
-      return todos.concat(action.todo);
-    case "REMOVE":
-      return todos.filter((todo) => todo.id !== action.id);
-    case "TOGGLE":
-      return todos.map((todo) =>
-        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
-      );
-    default:
-      return todos;
-  }
-}
+import React from "react";
+import ImmerComponent from "./chapter12/ImmerComponent";
 
 const App = () => {
-  /*const [todos, setTodos] = useState([
-    { id: 1, text: "리액트 기초 알아보기", checked: true },
-    { id: 2, text: "컴포넌트 스타일링해 보기", checked: true },
-    { id: 3, text: "일정 관리 앱 만들어 보기", checked: false },
-  ]);*/
-
-  //const [todos, setTodos] = useState(createBulkTodos);
-  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
-
-  const nextId = useRef(2501);
-
-  const onInsert = useCallback((text) => {
-    const todo = {
-      id: nextId.current,
-      text,
-      checked: false,
-    };
-    //setTodos((todos) => todos.concat(todo));
-    dispatch({ type: "INSERT", todo });
-    nextId.current += 1;
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    //setTodos((todos) => todos.filter((todo) => todo.id !== id));
-    dispatch({ type: "REMOVE", id });
-  }, []);
-
-  const onToggle = useCallback((id) => {
-    /*setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );*/
-    dispatch({ type: "TOGGLE", id });
-  }, []);
-
-  return (
-    <TodoTemplate>
-      <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-    </TodoTemplate>
-  );
+  return <ImmerComponent />;
 };
 
 export default App;

eade1334160a76ff5a3faa0d0c3fd29c2ad3f97a
SimHongSub <ghdtjq2038@gmail.com>
2021-01-26 21:23:14 +0900

    useState 함수형 업데이트와 immer 적용

--- src/chapter12/ImmerComponent.js
+++ src/chapter12/ImmerComponent.js
@@ -6,17 +6,14 @@ const ImmerComponent = () => {
   const [form, setForm] = useState({ name: "", username: "" });
   const [data, setData] = useState({ array: [], uselessValue: null });
 
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm(
-        produce(form, (draft) => {
-          draft[name] = value;
-        })
-      );
-    },
-    [form]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
 
   const onSubmit = useCallback(
     (e) => {
@@ -29,7 +26,7 @@ const ImmerComponent = () => {
       };
 
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         })
       );
@@ -41,22 +38,19 @@ const ImmerComponent = () => {
 
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1
-          );
-        })
-      );
-    },
-    [data]
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <div>

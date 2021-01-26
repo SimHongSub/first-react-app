6c6cc7f3f5ebf8b1b5e3c4191054cff8dcd32133
SimHongSub <ghdtjq2038@gmail.com>
2021-01-26 17:43:37 +0900

    컴포넌트 성능 최적화 - useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useReducer, useState, useRef, useCallback } from "react";
 import TodoTemplate from "./chapter10/TodoTemplate";
 import TodoInsert from "./chapter10/TodoInsert";
 import TodoList from "./chapter10/TodoList";
@@ -17,6 +17,21 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
   /*const [todos, setTodos] = useState([
     { id: 1, text: "리액트 기초 알아보기", checked: true },
@@ -24,7 +39,8 @@ const App = () => {
     { id: 3, text: "일정 관리 앱 만들어 보기", checked: false },
   ]);*/
 
-  const [todos, setTodos] = useState(createBulkTodos);
+  //const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   const nextId = useRef(2501);
 
@@ -34,20 +50,23 @@ const App = () => {
       text,
       checked: false,
     };
-    setTodos((todos) => todos.concat(todo));
+    //setTodos((todos) => todos.concat(todo));
+    dispatch({ type: "INSERT", todo });
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    //setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: "REMOVE", id });
   }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos((todos) =>
+    /*setTodos((todos) =>
       todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
       )
-    );
+    );*/
+    dispatch({ type: "TOGGLE", id });
   }, []);
 
   return (

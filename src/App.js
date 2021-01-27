43de24931927b950ef600e94826306a8d6e78849
SimHongSub <ghdtjq2038@gmail.com>
2021-01-27 14:54:36 +0900

    SPA 개발 - 라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import ImmerComponent from "./chapter12/ImmerComponent";
+import { Link, Route } from "react-router-dom";
+import About from "./chapter13/About";
+import Home from "./chapter13/Home";
 
 const App = () => {
-  return <ImmerComponent />;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
+    </div>
+  );
 };
 
 export default App;

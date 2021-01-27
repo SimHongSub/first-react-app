c290c06ff174af80c6ae2eb7ee0d8a03d360449f
SimHongSub <ghdtjq2038@gmail.com>
2021-01-27 16:34:37 +0900

    뉴스 뷰어 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
-import About from "./chapter13/About";
-import Home from "./chapter13/Home";
-import Profiles from "./chapter13/Profiles";
-import HistorySample from "./chapter13/HistorySample";
+import NewsList from "./chapter14/components/NewsList";
 
 const App = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필</Link>
-        </li>
-        <li>
-          <Link to="/history">History 예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다:</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;

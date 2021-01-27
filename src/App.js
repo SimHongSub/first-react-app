484ccd2298d930154e0f6c785c34cc955edc5f86
SimHongSub <ghdtjq2038@gmail.com>
2021-01-27 15:22:46 +0900

    SPA 개발 - URL 파라미터와 쿼리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import About from "./chapter13/About";
 import Home from "./chapter13/Home";
+import Profile from "./chapter13/Profile";
 
 const App = () => {
   return (
@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };

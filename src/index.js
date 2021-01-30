5543f0c83fcc01eb13d71cf311972605757c90fd
SimHongSub <ghdtjq2038@gmail.com>
2021-01-30 23:33:54 +0900

    리액트 애플리케이션에 리덕스 적용하기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./chapter17/reducers";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById("root")
 );
 

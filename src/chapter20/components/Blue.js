f2e13751729e1e77d9bdf49a7e8985d8ff549541
SimHongSub <ghdtjq2038@gmail.com>
2021-02-03 17:17:51 +0900

    서버 사이드 렌더링을 위한 프로젝트 준비

---
+++ src/chapter20/components/Blue.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Blue.scss";
+
+const Blue = () => {
+  return <div className="Blue">Blue</div>;
+};
+
+export default Blue;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import rootReducer, { rootSaga } from "./chapter18/modules";
-import { createLogger } from "redux-logger";
-import ReduxThunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const logger = createLogger();
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
-);
-sagaMiddleware.run(rootSaga);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </Provider>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

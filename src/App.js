e4c42e1396b843fe032873cd197986f7ca4235ce
SimHongSub <ghdtjq2038@gmail.com>
2021-02-05 15:22:43 +0900

    Users, UsersContainer 컴포넌트

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Menu from "./chapter20/components/Menu";
 import RedPage from "./chapter20/pages/RedPage";
 import BluePage from "./chapter20/pages/BluePage";
+import UsersPage from "./chapter20/pages/UsersPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
